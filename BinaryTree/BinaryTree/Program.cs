0211fd2dda32651c31a741d00bfe842b16fc95a4
merfields(LebedevKirill) <54494980+merfields@users.noreply.github.com>
2021-03-03 12:28:54 +0300

    BinaryTree

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tree exampleTree = new Tree();
+
+            exampleTree.Add(12);
+            exampleTree.Add(8);
+            exampleTree.Add(4);
+            exampleTree.Add(6);
+            exampleTree.Add(5);
+            exampleTree.Add(11);
+            exampleTree.Add(22);
+            exampleTree.Add(23);
+            exampleTree.Add(21);
+
+
+            exampleTree.PrintInOrder(exampleTree.rootNode);
+            
+
+            Console.WriteLine();
+
+            exampleTree.DeleteElem(22);
+            exampleTree.DeleteElem(6);
+
+            exampleTree.PrintInOrder(exampleTree.rootNode);
+
+
+            Console.WriteLine(exampleTree.Find(4));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+        class Tree
+        {
+
+            public class Node
+            {
+                public int exampleData;
+                public Node leftNode = null;
+                public Node rightNode = null;
+                public Node parent = null;
+                
+
+                public Node(int data)
+                {
+                    exampleData = data;
+                }
+
+            }
+
+        public Node rootNode;
+
+        public bool Add(int value)
+        {
+            Node before = null, after = this.rootNode;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.exampleData)
+                    after = after.leftNode;
+                else if (value > after.exampleData)
+                    after = after.rightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new Node(value);
+
+            if (this.rootNode == null)
+                this.rootNode = newNode;
+            else
+            {
+                if (value < before.exampleData)
+                    before.leftNode = newNode;
+                else
+                    before.rightNode = newNode;
+            }
+
+            return true;
+        }
+
+        public void PrintInOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                PrintInOrder(parent.leftNode);
+                Console.Write(parent.exampleData + " ");
+                PrintInOrder(parent.rightNode);
+            }
+        }
+        public void DeleteElem(int key)
+        {
+            Delete(rootNode, key);
+        }
+
+        Node Delete(Node root, int key)
+        {
+            if(root == null)
+            {
+                return root;
+            }
+
+            if (key < root.exampleData)
+            {
+                root.leftNode = Delete(root.leftNode, key);
+            }
+
+            else if (key > root.exampleData)
+            {
+                root.rightNode = Delete(root.rightNode, key);
+            }
+
+            else if (root.leftNode != null && root.rightNode != null)
+            {
+                root.exampleData = MinElem(root.rightNode);
+                root.rightNode = Delete(root.rightNode, root.exampleData);
+            }
+            else
+            {
+                if (root.leftNode != null)
+                {
+                    root = root.leftNode;
+                }
+                else if (root.rightNode != null)
+                {
+                    root = root.rightNode;
+                }
+                else root = null;
+            }
+            return root;
+        }
+
+        public bool Find(int value)
+            {
+                Node currentNode = rootNode;
+
+                while (currentNode != null)
+                {
+                    int compare = value - currentNode.exampleData;
+                    if (compare == 0)
+                        return true;
+
+                    if (compare < 0)
+                    {
+                        currentNode = currentNode.leftNode;
+                        continue;
+                    }
+                    currentNode = currentNode.rightNode;
+                }
+
+                return false;
+            }
+
+        private int MinElem(Node node)
+        {
+            while (node.leftNode != null)
+            {
+                node = node.leftNode;
+            }
+
+            return node.exampleData;
+        }
+    }
+ }
