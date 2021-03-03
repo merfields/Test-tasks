54b95390f6d7c981ba743aad187d80630c4a9ea9
merfields(LebedevKirill) <54494980+merfields@users.noreply.github.com>
2021-03-03 12:32:35 +0300

    LinkedList

---
+++ LnkdLst/LnkdLst/LinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LnkdLst
+{
+    class LinkedList
+    {
+        public class Node
+        {
+            public Node next;
+            public object exampleData;
+        }
+
+        private Node rootElement = null;
+
+        public Node FirstElement
+        {
+            get { return rootElement; }
+        }
+
+        public Node LastElement
+        {
+            get
+            {
+                Node current = rootElement;
+                if (current == null)
+                {
+                    return null;
+                }
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                return current;
+            }
+        }
+
+
+        public void DeleteElement(Node nodeToDelete)
+        {
+            if (rootElement == nodeToDelete)
+            {
+                rootElement = nodeToDelete.next;
+                nodeToDelete.next = null;
+            }
+            else
+            {
+                Node current = rootElement;
+
+                while (current.next != null)
+                {
+                    if (current.next == nodeToDelete)
+                    {
+                        current.next = nodeToDelete.next;
+                        nodeToDelete.next = null;
+                        break;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+
+
+        public void AddElement(object value)
+        {
+            Node newNode = new Node { exampleData = value };
+            if (rootElement == null)
+            {
+                rootElement = newNode;
+            }
+            else LastElement.next = newNode;
+        }
+
+        public void PrintElements()
+        {
+            Node current = rootElement;
+
+            while (current != null)
+            {
+                Console.Write(current.exampleData);
+                current = current.next;
+            }
+        }
+
+        public void ReverseElements()
+        {
+            Node prevNode = null;
+            Node curNode = rootElement;
+            Node nextNode = rootElement.next;
+
+            while(nextNode != null)
+            {
+                curNode.next = prevNode;
+                prevNode = curNode;
+                curNode = nextNode;
+                nextNode = curNode.next;
+            }
+            curNode.next = prevNode;
+            rootElement = curNode;
+        }
+
+
+    }
+}
---
+++ LnkdLst/LnkdLst/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LnkdLst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList exampleList = new LinkedList();
+
+            exampleList.AddElement(1);
+            exampleList.AddElement(2);
+            exampleList.AddElement(3);
+            exampleList.AddElement(4);
+            exampleList.AddElement(5);
+            exampleList.AddElement(6);
+
+            exampleList.PrintElements();
+            Console.WriteLine();
+
+            exampleList.DeleteElement(exampleList.FirstElement);
+            exampleList.PrintElements();
+            Console.WriteLine();
+
+            exampleList.DeleteElement(exampleList.LastElement);
+            exampleList.PrintElements();
+            Console.WriteLine();
+
+            exampleList.ReverseElements();
+            exampleList.PrintElements();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
